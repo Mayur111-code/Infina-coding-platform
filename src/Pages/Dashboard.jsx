f5c7deb906cf3a6d8537231dd7f2da8ab6ec72c4
Mayur111-code <mayurborse7440@gmail.com>
2025-09-22 22:50:57 +0530

    update code

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -26,7 +26,7 @@ function Dashboard() {
 
     // Fetch all questions from JSON server
     // axios.get("http://localhost:3000/challenges") // for development
-    axios.get("https://json-server-infinacode.onrender.com") // for vercle
+     axios.get("https://json-server-infinacode.onrender.com") // for vercle
       .then((res) => {
         const questionsArray = res.data;
         setAllQuestionsCount(questionsArray.length);
