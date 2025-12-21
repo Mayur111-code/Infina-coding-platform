c3f9acb3788fe1960bb82166b66ec566271e0535
Mayur Borse 👨🏻‍💻 <mayurborse7440@gmail.com>
2025-12-21 21:43:06 +0530

    Remove emoji from server startup log message

--- backend/index.js
+++ backend/index.js
@@ -65,5 +65,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Server running on PORT: ${PORT}`);
+  console.log(`Server running on PORT: ${PORT}`);
 });
